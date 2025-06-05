1566740cb0aac524a460b70917b9d1be0df68dd0
Alexandre Abreu <alexairor602@gmail.com>
2025-06-05 19:31:37 -0300

    Delete CartaoCredito[1].java

--- CartaoCredito[1].java
+++
@@ -1,6 +0,0 @@
-public class CartaoCredito implements Pagamento {
-    @Override
-    public void realizarPagamento(double valor) {
-        System.out.println("Pagamento de R$ " + valor + " realizado com Cartão de Crédito.");
-    }
-}
