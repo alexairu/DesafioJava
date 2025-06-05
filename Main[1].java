d726ceafa1ce54ef602043796a2c56ba0e827f20
Alexandre Abreu <alexairor602@gmail.com>
2025-06-05 19:31:44 -0300

    Delete Main[1].java

--- Main[1].java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Pagamento pagamentoCartao = new CartaoCredito();
-        Pagamento pagamentoBoleto = new BoletoBancario();
-
-        pagamentoCartao.realizarPagamento(150.75);
-        pagamentoBoleto.realizarPagamento(89.90);
-    }
-}
