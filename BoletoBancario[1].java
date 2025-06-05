49f31e65b2577b230fb7b8cffa51735397761758
Alexandre Abreu <alexairor602@gmail.com>
2025-06-05 19:31:16 -0300

    Delete BoletoBancario[1].java

--- BoletoBancario[1].java
+++
@@ -1,6 +0,0 @@
-public class BoletoBancario implements Pagamento {
-    @Override
-    public void realizarPagamento(double valor) {
-        System.out.println("Pagamento de R$ " + valor + " realizado com Boleto Bancário.");
-    }
-}
