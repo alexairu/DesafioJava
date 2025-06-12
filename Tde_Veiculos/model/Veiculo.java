2f2c1e16ca03e63f5d5e8bc72e8b5a846f065182
Alexandre Abreu <alexairor602@gmail.com>
2025-06-12 11:06:35 -0300

    Add files via upload

---
+++ Tde_Veiculos/model/Veiculo.java
@@ -0,0 +1,23 @@
+package veiculos.model;
+
+import veiculos.interfaces.Exibivel;
+
+public abstract class Veiculo implements Exibivel {
+    protected String placa;
+    protected String modelo;
+    protected int ano;
+
+    public Veiculo(String placa, String modelo, int ano) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public String getPlaca() { return placa; }
+    public String getModelo() { return modelo; }
+    public int getAno() { return ano; }
+
+    public void setPlaca(String placa) { this.placa = placa; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+    public void setAno(int ano) { this.ano = ano; }
+}
